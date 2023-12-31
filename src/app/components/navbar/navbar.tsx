b03142ca49e31a7b9cceda6b17b0835aa5f7ffba
David Dasenbrook <david.dasenbrook@tngtech.com>
2023-12-31 12:01:49 +0100

    new logo for dark mode

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -3,7 +3,14 @@ import { MenuItem } from "./menuitem";
 
 export function NavBar() {
   return <nav className="flex flex-row items-center">
-    <div className="p-2 lg:p-8"><Link href="/"><img src="/logo.png" alt="Hobbymusik" className="w-96" /></Link></div>
+    <div className="p-2 lg:p-8">
+      <Link href="/">
+      <picture className="w-96">
+        <source srcSet="/logo-dark.png" media="(prefers-color-scheme: dark)" />
+        <img src="/logo.png" alt="Hobbymusik" />
+      </picture>
+      </Link>
+    </div>
     <ul className="font-bold text-xl">
       <MenuItem href="/releases">Releases</MenuItem>
       <MenuItem href="#">Shop</MenuItem>
