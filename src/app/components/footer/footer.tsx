e1655bf5b7ab03d5fea21547245697702fb60fa3
David Dasenbrook <david.dasenbrook@tngtech.com>
2024-10-20 16:44:28 +0200

    update footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export function Footer() {
-  return <footer className="text-center p-2 text-sm">
+  return <footer className="text-center lg:text-left lg:ml-[20rem] mt-16 text-sm">
     <span>(C) { new Date().getFullYear() } hobbymusik | </span>
     <Link href="/datenschutz">Datenschutz</Link>
   </footer>;
