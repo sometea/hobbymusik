61933204e7eb2905b0cf72676942db0eee38bf3c
David Dasenbrook <david.dasenbrook@tngtech.com>
2024-10-27 13:53:18 +0100

    update contact text

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,15 +1,28 @@
 export default function Contact() {
   return <div>
-    <div className="mb-8">
-    Wenn du an einer Zusammenarbeit interessiert bist, eine unserer Künstler:innen buchen willst,
-    Lob oder Feedback loswerden möchtest oder einfach mal hallo sagst:
-    Kontaktiere uns per E-Mail unter info at hobbymusik punkt net.
+    <div className="m-8 lg:mx-0">
+      <p className="mb-4">
+        Falls du Hobbymusik magst und uns das sagen willst, kontaktiere uns am
+        besten per E-Mail. Falls du konstruktive Kritik loswerden willst oder
+        weitere Fragen zu uns, unseren Releases oder unseren Künstler*innen hast,
+        auch.
+      </p>
+      <p className="mb-4">
+        Vielleicht bist du oder Künstler*innen, die du kennst, auch an einer
+        Zusammenarbeit mit uns interessiert? Wenn dir unser Konzept gefällt und du
+        meinst, dass wir zusammen passen könnten, schreibe uns auch und füge am besten
+        gleich einen Link zu deiner Musik oder Kunst hinzu. Bitte schicke keine großen
+        Dateien (wie mp3s) per E-Mail.
+      </p>
+      <p className="mb-4">
+        Unsere Adresse lautet: info at hobbymusik punkt net.
+      </p>
     </div>
     <div className="text-xs">
-    Inhaltlich verantwortlich: David Dasenbrook, Sachsenring 67, 50677 Köln. 
-    Haftungshinweis: Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir 
-    keine Haftung für die Inhalte externer Links. 
-    Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+      Inhaltlich verantwortlich: David Dasenbrook, Sachsenring 67, 50677 Köln.
+      Haftungshinweis: Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir
+      keine Haftung für die Inhalte externer Links.
+      Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
     </div>
   </div>;
 }
